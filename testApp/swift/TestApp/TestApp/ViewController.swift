c1fb68c4d6113d498f64291741fe58b4d36b3851
Tao Xu <taox@fb.com>
2019-08-28 14:36:56 -0700

    Update(auto commit)

--- testApp/swift/TestApp/TestApp/ViewController.swift
+++ testApp/swift/TestApp/TestApp/ViewController.swift
@@ -7,13 +7,23 @@
 //
 
 import UIKit
-//import PytorchExpObjC
 
 class ViewController: UIViewController {
-
+    var predictor: Predictor?
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        if let modelPath = Bundle.main.path(forResource: "resnet18", ofType: "pt") {
+            self.predictor = Predictor(modelPath: modelPath)
+        }
+        let image = UIImage(named: "wolf_400x400.jpg")!
+        let resizedImage = image.resized(to: CGSize(width: 224, height: 224))
+        
+        predictor?.predict(image: resizedImage, completion: { results in
+            for result in results {
+                print("-[score]: \(result.score), -[label]: \(result.label)")
+            }
+        })
     }
 }
 
