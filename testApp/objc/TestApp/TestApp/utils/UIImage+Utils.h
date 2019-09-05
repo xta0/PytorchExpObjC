e0a52c68a8a0844c089becf1e86d3bb461dff63a
Tao Xu <taox@fb.com>
2019-09-05 00:42:26 -0700

    Update(auto commit)

--- testApp/objc/TestApp/TestApp/utils/UIImage+Utils.h
+++ testApp/objc/TestApp/TestApp/utils/UIImage+Utils.h
@@ -9,20 +9,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface UIImage (Utils)
 
-
-/**
- Resize the image with the given size
-
- @param sz The new size
- @return A new UIImage object with the given size
- */
 - (UIImage* )resize:(CGSize)sz;
-
-/**
- <#Description#>
-
- @return <#return value description#>
- */
 - (float* )normalizedBuffer;
 
 @end
